489b543208efa2f99e8283b8e1950bac971dcd56
GuerrillaWarfare <gwf@gwf.ninja>
2015-06-05 10:52:51 -0400

    Refactored a bit of code.

--- core/info.py
+++ core/info.py
@@ -18,36 +18,21 @@ def Help():
 
     Loot - Extract sensitive information from a file.
 
-    Usage: ./loot.py -f [FILE] [EXTRACT-OPTION]
+    Usage: ./loot.py -f [FILE] [OPTION]
 
-    ./loot.py -f [FILE] --btc | Grab bitcoin addresses if in file.
+    Options:
 
-    ./loot.py -f [FILE] --mac | Grab MAC addresses if any are in file.
-
-    ./loot.py -f [FILE] --iat | Grab instagram access tokens if any are in file.
-
-    ./loot.py -f [FILE] --ssn | Grab social security numbers if any are in file.
-
-    ./loot.py -f [FILE] --ccn | Grab credit card numbers if any are in file.
-
-    ./loot.py -f [FILE] --ipv6 | Grab IPv6 addresses if any are in file.
-
-    ./loot.py -f [FILE] --ipv4 | Grab IPv4 addresses if any are in file.
-
-    ./loot.py -f [FILE] --email | Grab Email addresses if any are in file.
-
-    ./loot.py -f [FILE] --hash | Grab hash values if any are in file.
-
-    ./loot.py -f [FILE] --phn | Grab phone numbers if any are in file.
-
-    What can it extract ?:
-
-    1. Links
-    2. hash values
-    3. email addresses
-    4. ipv4, ipv6 addresses
-    5. instagram access tokens
-    6. bitcoin wallet addresses
-    7. MAC addresses with : or - (deliminators)
-    8. USA Based Telephone, Social Security and Major Credit Card numbers.
+    --btc   | Grab bitcoin addresses if any are in file.
+    --blid  | Grab Blockchain Identifiers if any are in file.
+    --fat   | Grab Facebook Access Tokens if any are in file.
+    --mac   | Grab MAC addresses if any are in file.
+    --iat   | Grab instagram access tokens if any are in file.
+    --fat   | Grab facebook access tokens if any are in file
+    --ssn   | Grab social security numbers if any are in file.
+    --ccn   | Grab credit card numbers if any are in file.
+    --ipv6  | Grab IPv6 addresses if any are in file.
+    --ipv4  | Grab IPv4 addresses if any are in file.
+    --email | Grab Email addresses if any are in file.
+    --hash  | Grab hash values if any are in file.
+    --phn   | Grab phone numbers if any are in file.
     """
